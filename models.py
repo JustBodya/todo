c99836e306426720a74d4c129e2dde50959ec7d0
JustBodya <147643172+JustBodya@users.noreply.github.com>
2024-06-09 01:27:16 +0300

    Update models.py

--- models.py
+++ models.py
@@ -9,8 +9,13 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), nullable=False)
     description = db.Column(db.String(200))
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    created_at = db.Column(db.DateTime,
+                           default=datetime.utcnow,
+                          )
+    updated_at = db.Column(db.DateTime,
+                           default=datetime.utcnow,
+                           onupdate=datetime.utcnow,
+                          )
 
 
     def to_dict(self):
@@ -19,5 +24,5 @@ class Task(db.Model):
             'title': self.title,
             'description': self.description,
             'created_at': self.created_at,
-            'updated_at': self.updated_at
+            'updated_at': self.updated_at,
         }
